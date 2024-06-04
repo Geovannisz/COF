28fe70bdd6488f8aff2ed8abc0bbbb7926074b13
Geovannisz <geovanni@usp.br>
2024-06-04 03:52:37 -0300

    .

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-function togglePricing() {
-  const toggle = document.querySelector('.toggle');
-  const isAnnual = toggle.classList.toggle('annual');
-  document.querySelectorAll('.mensal').forEach(el => el.style.display = isAnnual ? 'none' : 'block');
-  document.querySelectorAll('.anual').forEach(el => el.style.display = isAnnual ? 'block' : 'none');
-  document.querySelectorAll('.mensal-link').forEach(el => el.style.display = isAnnual ? 'none' : 'block');
-  document.querySelectorAll('.anual-link').forEach(el => el.style.display = isAnnual ? 'block' : 'none');
-}
